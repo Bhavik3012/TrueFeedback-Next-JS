c6ebd0ffb9dc6e3e31aa91912ceff61ed85aeb62
Bhavik3012 <bhaviksongara301205@gmail.com>
2026-01-10 23:38:25 +0530

    fix: remove success log from database connection

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -15,8 +15,6 @@ async function dbConnect(): Promise<void> {
     const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
 
     connection.isConnected = db.connections[0].readyState;
-
-    console.log("DB connected successfully");
   } catch (error) {
     console.log("Database connection faield ", error);
     process.exit(1);
